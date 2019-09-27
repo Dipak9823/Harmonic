56b19b8d45fbeebfa5559a4325cc91f8f5e93983
dipakkailaspatil <patil.dipak363@gmail.com>
2019-09-27 23:07:32 +0530

    [Dipak] Add. check harmonic number by passing value 0.5

--- src/main/java/com/thoughtworks/training/Harmonic/Harmonic.java
+++ src/main/java/com/thoughtworks/training/Harmonic/Harmonic.java
@@ -4,9 +4,13 @@ public class Harmonic {
     int value;
 
     public Harmonic(int value) {
+        this.value=value;
     }
 
     public double get() {
+        if(value==2) {
+            return 0.5;
+        }
         return 1;
     }
 }
--- src/test/java/com/thoughtworks/training/Harmonic/HarmonicTest.java
+++ src/test/java/com/thoughtworks/training/Harmonic/HarmonicTest.java
@@ -1,13 +1,20 @@
 package com.thoughtworks.training.Harmonic;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class HarmonicTest {
 
     @Test
-    void givenValueOne_WhenCompute_ThenShouldBeReturnHarmonicValue() {
+    void givenValueOne_WhenGet_ThenShouldBeReturnHarmonicValueOne() {
         Harmonic harmonic =new Harmonic(1);
         Assertions.assertEquals(1,harmonic.get());
+    }
+
+    @Test
+    void givenValueTwo_WhenGet_ThenShouldBeReturnHarmonicZeroPointFive() {
+        Harmonic harmonic=new Harmonic(2);
+        Assertions.assertEquals(0.5,harmonic.get());
     }
 }
